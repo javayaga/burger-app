faf2f7019547033b96585ad98b913123ebbf4f98
Alice Russell <alice.louise.russell@gmail.com>
2020-01-07 10:51:47 -0500

    orm

--- config/orm.js
+++ config/orm.js
@@ -1,15 +1,36 @@
 // require connection
+const connection = require("../config/connection");
 
+// SQL statement functions
+const orm = {
+    // selectAll()
+    selectAll: function(tableInput) {
+        const queryString = "SELECT * FROM burgers";
 
-// helper functions
-// selectAll()
+        connection.query(queryString, [tableInput], function(err, result) {
+        if (err) throw err;
 
+        callback(result);
+        });
+    },
 
-// insertOne()
+    // insertOne()
+    insertOne: function(burger_name, devoured) {
+        const queryString = "INSERT INTO burgers (burger_name, devoured) VALUES (?, ?)";
 
+        connection.query(queryString, [burger_name, devoured], function(err, results) {
+        if (err) throw err;
 
-// updateOne()
+        callback(result);
+        });
+    }
 
+    // updateOne()
+    
+
+};
 
 
 // export orm object
+module.exports = orm;
+
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,11 +1,21 @@
 // require express 
+const express = require("express");
 
 // router
+const router = express.Router();
 
 // require burger model
+const burger = require("../models/burger");
+
 
 // ROUTES
+// get all burgers
 
+// create a burger 
 
+// update a burger 
+
+// delete a burger
 
 // export router
+module.exports = router;
