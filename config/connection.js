93e305201f008c852aa2206718dc800ed0657c49
Alice Russell <alice.louise.russell@gmail.com>
2020-01-07 11:03:36 -0500

    db connecion

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,22 @@
-// require mysql
+// Set up MySQL connection.
+var mysql = require("mysql");
 
-// create connection to db
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "19911991",
+  database: "burgers_db"
+});
 
-// make connection
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
-// export connection
+// Export connection for our ORM to use.
+module.exports = connection;
