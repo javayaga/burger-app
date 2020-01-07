a137c8c2dbf5f01f674771497c85f45df37ca28d
Alice Russell <alice.louise.russell@gmail.com>
2020-01-07 10:16:49 -0500

    server created

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 // dependencies 
+const express = require("express");
 
 // PORT
+const PORT = process.env.PORT || 8080;
 
 // express
+const app = express();
+
+// serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// parse application body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // handlebars 
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // import routes 
+const routes = require("./controllers/burgers_controller");
+
+app.use(routes);
 
 // start server 
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
 
 
